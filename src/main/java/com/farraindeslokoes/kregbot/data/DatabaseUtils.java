315a9c6a5f58315107d319cb592c252aa7f1a701
Nukeologist <guilhermebignelli@terra.com.br>
2019-02-06 00:37:52 -0200

    new try using prepared statements

--- src/main/java/com/farraindeslokoes/kregbot/data/DatabaseUtils.java
+++ src/main/java/com/farraindeslokoes/kregbot/data/DatabaseUtils.java
@@ -32,42 +32,5 @@ public class DatabaseUtils {
 
     }
 
-    public static ResultSet getResultSet(String tableName, String Collumn1name, String Collumn2name, String Collumn2) {
-        Connection connection = KregBot.getSQLConnection();
-
-        try {
-            Statement stmt = connection.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT " + Collumn1name + ", " + Collumn2name + " FROM " + tableName + " WHERE " + Collumn2name + " = '" + Collumn2 + "';");
-            return rs;
-        } catch (SQLException e) {
-            System.out.println("Failed to connect/ connection doesnt exist / database error when getting result");
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public static void insertRowIntoTable(String table, String row, String Column1, String Column2) {
-        Connection connection = KregBot.getSQLConnection();
-
-        try {
-            Statement stmt = connection.createStatement();
-            stmt.executeUpdate("INSERT INTO " + table + "(" + Column1 + ", " + Column2 +  ") VALUES " + row);
-        } catch (SQLException e) {
-            System.out.println("Failed to connect/ connection doesnt exist / database error when inserting");
-            e.printStackTrace();
-        }
-    }
-
-    public static void updateRowInTable(String table, String whereCollumn, String where, String updateCollumn, String update) {
-        Connection connection = KregBot.getSQLConnection();
-
-        try {
-            Statement stmt = connection.createStatement();
-            stmt.executeUpdate("UPDATE " + table + " SET " + updateCollumn + " = " + update + " WHERE " + whereCollumn + " = '" + where + "';");
-        } catch (SQLException e) {
-            System.out.println("Failed to connect/ connection doesnt exist / database error when updating");
-            e.printStackTrace();
-        }
-    }
 
 }
--- src/main/java/com/farraindeslokoes/kregbot/events/HelloEvent.java
+++ src/main/java/com/farraindeslokoes/kregbot/events/HelloEvent.java
@@ -1,11 +1,11 @@
 package com.farraindeslokoes.kregbot.events;
 
+import com.farraindeslokoes.kregbot.KregBot;
 import com.farraindeslokoes.kregbot.data.DatabaseUtils;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -41,30 +41,80 @@ public class HelloEvent extends ListenerAdapter {
         //WARNING: EXPERIMENTAL CODE
         //TODO: SANITIZE USER INPUT
         Matcher matcher = INCREMENT_DECREMENT.matcher(received[0]);
+
         if (matcher.matches()) {
             String key = matcher.group(1);
             int incr = matcher.group(2).equals("++") ? 1 : -1;
-            DatabaseUtils.createTableIfNotExists("increments", "( message varchar(45) NOT NULL, number integer NOT NULL DEFAULT '0' );");
-            ResultSet set = DatabaseUtils.getResultSet("increments", "number", "message", key );
-            if (set == null) return;
-            try {
-                if ( set.next() ) {
-                    
-                    int number = set.getInt(1) + incr;
-                    DatabaseUtils.updateRowInTable("increments", "message", key, "number", Integer.toString(number) );
-                    event.getChannel().sendMessage(key +  " == " + number).queue();
-                    System.out.println("tried to update table");
-                }else {
-                    DatabaseUtils.insertRowIntoTable("increments", "(" + key + ", " + incr + ");", "message", "number");
-                    event.getChannel().sendMessage(key + " == " + incr).queue();
-                    System.out.println("created new row");
+            DatabaseUtils.createTableIfNotExists("increments", "( message varchar(45) NOT NULL UNIQUE, number integer NOT NULL DEFAULT '0' );");
+            //getIncrements();
+            String SQL = "SELECT message, number " + "FROM increments " + "WHERE message = ?";
+
+            try (Connection conn = KregBot.getSQLConnection();
+                 PreparedStatement pstmt = conn.prepareStatement(SQL)) {
+
+                pstmt.setString(1, key);
+                ResultSet rs = pstmt.executeQuery();
+
+                while (rs.next()) {
+
+                    if (rs.getString("message").equals(key)) {
+                        int newNumber = rs.getInt("number") + incr;
+                        updateDabase(key, newNumber);
+                    }
                 }
-            } catch (SQLException e) {
-                    event.getChannel().sendMessage("SQL problem...").queue();
-                    e.printStackTrace();
+                insertToDatabase(key, incr);
+                getIncrements();
+            } catch (SQLException ex) {
+                System.out.println("failed to query");
+                ex.printStackTrace();
             }
+
         }
 
 
+    }
+
+    public void getIncrements() {
+
+        String SQL = "SELECT message, number FROM increments";
+
+        try (Connection conn = KregBot.getSQLConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(SQL)) {
+            // display increment information
+            displayIncrement(rs);
+
+        } catch (SQLException ex) {
+           ex.printStackTrace();
+        }
+    }
+
+    private void displayIncrement(ResultSet rs) throws SQLException {
+        while (rs.next()) {
+            System.out.println(rs.getString("message") + "\t"
+                    + rs.getInt("first_name"));
+
+        }
+    }
+
+    private void insertToDatabase(String message, int number) throws SQLException {
+        String SQL = "INSERT INTO increments(message, number) " + "VALUES(?,?) ON CONFLICT DO NOTHING";
+
+        Connection conn = KregBot.getSQLConnection();
+        PreparedStatement pstmt = conn.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
+
+        pstmt.setString(1, message);
+        pstmt.setInt(2, number);
+        pstmt.executeUpdate();
+    }
+
+    private void updateDabase(String message, int number) throws SQLException {
+        String SQL = "UPDATE increments " + "SET number = ? " + "WHERE message = ?";
+        Connection conn = KregBot.getSQLConnection();
+        PreparedStatement pstmt = conn.prepareStatement(SQL);
+
+        pstmt.setInt(1, number);
+        pstmt.setString(2, message);
+        pstmt.executeUpdate();
     }
 }
