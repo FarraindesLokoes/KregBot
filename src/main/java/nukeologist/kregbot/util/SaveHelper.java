f79b1cb6fca508873a98ef47450b6dc44a8b09b7
Nukeologist <guilhermebignelli@terra.com.br>
2019-07-08 18:37:39 -0300

    first saving utility

--- src/main/java/nukeologist/kregbot/util/SaveHelper.java
+++ src/main/java/nukeologist/kregbot/util/SaveHelper.java
@@ -1,14 +1,44 @@
 package nukeologist.kregbot.util;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import nukeologist.kregbot.KregBot;
+
+import java.io.*;
 
 /**
  * Class to help with saving data
  */
-public class SaveHelper {
+public class SaveHelper<T> {
 
-    public static void trySaveGson() {
-        Gson gson = new Gson();
+    private final Class<T> type;
+
+    public SaveHelper(Class<T> type) {
+        this.type = type;
     }
 
+    public boolean saveJson(T object, String fileName) {
+        try (Writer writer = new FileWriter(fileName + ".json")) {
+            Gson gson = new GsonBuilder().create();
+            gson.toJson(object, writer);
+        } catch (IOException e) {
+            KregBot.LOG.info("failed to save json with filename {}.json", fileName);
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public T fromJson(String fileName) {
+        T obj;
+        try (Reader reader = new FileReader(fileName + ".json")) {
+            Gson gson = new GsonBuilder().create();
+            obj = gson.fromJson(reader, type);
+        }catch(IOException e) {
+            KregBot.LOG.info("failed to save json with filename {}.json", fileName);
+            e.printStackTrace();
+            return null;
+        }
+        return obj;
+    }
 }
