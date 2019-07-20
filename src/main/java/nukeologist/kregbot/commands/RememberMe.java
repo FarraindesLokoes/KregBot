a05030f4a98d084a82ef6652b6ba3264505255e2
SpicyFerret <danilo.nmarques@gmail.com>
2019-07-20 19:34:17 -0300

    RememberMe command added

---
+++ src/main/java/nukeologist/kregbot/commands/RememberMe.java
@@ -0,0 +1,84 @@
+package nukeologist.kregbot.commands;
+
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.User;
+import nukeologist.kregbot.api.Command;
+import nukeologist.kregbot.api.Context;
+import nukeologist.kregbot.util.MessageHelper;
+
+import java.util.ArrayList;
+
+/**
+ * @autor SpicyFerret
+ *
+ * class that will make an alarm to trigger on specified time
+ * */
+
+public class RememberMe {
+    private static ArrayList<Thread> clocks = new ArrayList<>();
+
+    @Command("rememberMe")
+    public static void setAlarm(Context context){
+        String[] words = context.getWords();
+        if (words.length == 1) {
+            context.reply("Syntax error: timer and message(optional) not specified");
+        } else if (words.length == 2) {
+            addClock(new Clock(null, Integer.getInteger(words[1]), context.getChannel(), context.getAuthor()));
+        } else if (words.length > 2) {
+            addClock(new Clock(MessageHelper.collapse(words, 2), Integer.getInteger(words[1]), context.getChannel(), context.getAuthor()));
+        }
+
+    }
+
+    private static void addClock(Clock clock) {
+        Thread thread = new Thread(clock);
+        thread.start();
+        clocks.add(thread);
+    }
+
+    static void clearClock(Clock clock) {
+        clocks.remove(clock);
+    }
+}
+/**
+ * @autor SpicyFerret
+ *
+ * helper class
+ * */
+class Clock implements Runnable {
+    private long timer;
+    private String alarm;
+    private MessageChannel channel;
+    private long setTime;
+    private User user;
+
+    Clock(String alarm, long timer, MessageChannel channel, User user){
+        this.alarm = alarm;
+        this.timer = timer * 1000;
+        this.channel = channel;
+        this.setTime = System.currentTimeMillis();
+        this.user = user;
+    }
+
+    private void fire(){
+        if (alarm != null) {
+            channel.sendMessage(user + ", it`s time to: \n" + alarm).queue();
+        } else {
+            channel.sendMessage(user + ", it`s time!").queue();
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(timer);
+            fire();
+        } catch (InterruptedException e) {
+            channel.sendMessage(user + "\n" +"```Alarm" + alarm + "failed! \n" +
+                    (System.currentTimeMillis() - setTime) / 1000 + "seconds elapsed \n" +
+                    (timer - (System.currentTimeMillis() - setTime)) / 1000 + "seconds remaining!```").queue();
+        }
+        RememberMe.clearClock(this);
+    }
+
+}
