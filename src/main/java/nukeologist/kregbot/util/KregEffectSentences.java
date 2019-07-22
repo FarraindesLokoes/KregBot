e2bc4eb4015c09367b39a2641fcf231113768751
SpicyFerret <danilo.nmarques@gmail.com>
2019-07-22 01:06:20 -0300

    Kreg Effect Sentences

---
+++ src/main/java/nukeologist/kregbot/util/KregEffectSentences.java
@@ -0,0 +1,18 @@
+package nukeologist.kregbot.util;
+
+import java.util.Random;
+
+public class KregEffectSentences {
+
+    private static String[] sentences = {};
+    private static Random random;
+
+    public static String getRandonSentance(){
+        return sentences[random.nextInt(sentences.length)];
+    }
+
+    public static String[] getAllSentences(){
+        return sentences;
+    }
+
+}
