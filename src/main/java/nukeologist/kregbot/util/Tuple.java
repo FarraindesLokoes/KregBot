76c8f28b6a01249421638845f10f6741496c1cd8
SpicyFerret <danilo.nmarques@gmail.com>
2019-09-23 21:34:26 -0300

    Tuple added

---
+++ src/main/java/nukeologist/kregbot/util/Tuple.java
@@ -0,0 +1,11 @@
+package nukeologist.kregbot.util;
+
+public class Tuple<A, B> {
+    public A a;
+    public B b;
+
+    public Tuple(A a, B b) {
+        this.a = a;
+        this.b = b;
+    }
+}
