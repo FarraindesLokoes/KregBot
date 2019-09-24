7023bc65a66aef633992aec7182c3b9495cee940
SpicyFerret <danilo.nmarques@gmail.com>
2019-09-24 16:12:23 -0300

    replaceClear added

--- src/main/java/nukeologist/kregbot/commands/ReplaceWordCommand.java
+++ src/main/java/nukeologist/kregbot/commands/ReplaceWordCommand.java
@@ -2,6 +2,7 @@ package nukeologist.kregbot.commands;
 
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.MessageBuilder;
+import net.dv8tion.jda.api.Permission;
 import nukeologist.kregbot.api.Command;
 import nukeologist.kregbot.api.CommandHelp;
 import nukeologist.kregbot.api.Context;
@@ -54,6 +55,16 @@ public class ReplaceWordCommand {
            ReplacerListener.VALUES.remove(context.getMember().getGuild().getIdLong(), words[1]);
            context.getMessage().addReaction(Constants.Emotes.ItsApproved).queue();
         }
+    }
+
+    @Command(value = "replaceClear", type = ContextType.GUILD)
+    public static void replaceClear(Context context) {
+        if (context.getMember().hasPermission(Permission.ADMINISTRATOR)) {
+            ReplacerListener.VALUES.getMap().clear();
+            context.reply(", you nazi!");
+            return;
+        }
+        context.reply(", you does not have permission to do that, fucking cunt.");
     }
 
     @Command(value = "replaceTable", type = ContextType.GUILD)
