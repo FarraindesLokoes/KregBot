ebf61b60e438d10ad6bfdc82352351baf480e65f
SpicyFerret <danilo.nmarques@gmail.com>
2019-10-09 22:22:06 -0300

    version update

--- src/main/java/nukeologist/kregbot/KregBot.java
+++ src/main/java/nukeologist/kregbot/KregBot.java
@@ -17,7 +17,7 @@ public enum KregBot {
 
     INSTANCE;
 
-    private static final String VERSION = "1.0.1";
+    private static final String VERSION = "1.1.0";
 
     public static final Logger LOG = LoggerFactory.getLogger("[KregBot|Reborn]");
 
